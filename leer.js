d6368a5edf8a456a2edc94cc98543280dd80dbb4
juannchin <juannchin@users.noreply.github.com>
2022-04-09 01:42:15 -0600

    first commit

---
+++ leer.js
@@ -0,0 +1,28 @@
+const getValueInput = () =>{
+
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function() {
+        if(req.readyState == 4) {
+            var response = JSON.parse(req.responseText);
+            document.getElementById('container').innerHTML = '<img id="pic" src=""  width="100%" /> ';
+            document.getElementById("pic").src = response.url;
+        } else {
+            document.getElementById('container').innerHTML = '<div class="loader"></div>';
+        }
+    }
+
+    var url = "https://api.nasa.gov/planetary/earth/assets?";
+    var lat = document.getElementById("Latitud").value;
+    var lon = document.getElementById("Longitud").value;
+    var api_key = "PT9xDosnbfG4Re08XZOaZTRxaBMCFEr2a6fSrYcm";
+
+    req.open("GET", url + 
+    "lon="+ lon + 
+    "&lat=" + lat + 
+    "&date=2021-02-01&dim=0.1" + 
+    "&api_key=" + api_key);
+
+    req.send();
+
+}
